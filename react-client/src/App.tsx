91b384618c6777f258339cf5d66348edcc1b7cb2
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-07-24 18:57:58 -0300

    feat: initial integration with API

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useUsers } from "./graphql/queries/user";
 
-function App() {
+const App = () => {
+  const { status, data, error, isFetching }: any = useUsers();
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Posts</h1>
+      <div>
+        {status === "loading" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <div>Hello world</div>
+            <div>
+              {data.map((user: any) => (
+                <div key={user.id}>
+                  <h1>{user.name}</h1>
+                  <h1>{user.phone}</h1>
+                </div>
+              ))}
+            </div>
+            <div>{isFetching ? "Background Updating..." : " "}</div>
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
--- react-client/src/index.tsx
+++ react-client/src/index.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from "react-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
