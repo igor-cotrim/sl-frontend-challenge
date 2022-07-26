30bc3022abd7456b03b9cb53150a3876759b7ad3
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-07-25 21:30:36 -0300

    feat: create component Content

---
+++ react-client/src/components/Content/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styled'
+
+type ContentProps = {
+  children: React.ReactChild
+}
+
+const Content = ({ children }: ContentProps) => {
+  return (
+    <S.Container>
+      {children}
+    </S.Container>
+  )
+}
+
+export default Content
---
+++ react-client/src/components/Content/styled.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #1b1b1b;
+`
