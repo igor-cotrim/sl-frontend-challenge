dc143855242ae769a6e4a8eb61465efc694f7065
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-07-25 21:27:52 -0300

    feat: create reset css global and import necessary fonts

--- react-client/src/index.tsx
+++ react-client/src/index.tsx
@@ -3,13 +3,16 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { Global } from "@emotion/react";
 
 import App from "./App";
+import { GlobalStyle } from "./styles/global";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
+    <Global styles={GlobalStyle} />
     <App />
   </QueryClientProvider>,
   document.getElementById("root")
