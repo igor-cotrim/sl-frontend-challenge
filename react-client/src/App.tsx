11d78f2e8eba95d90992b4cbaf8bc6fa8427ec85
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-07-25 21:31:57 -0300

    feat: add Components in file App.tsx

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,33 +1,9 @@
-import { useUsers } from "./graphql/queries/user";
+import { Content, List } from "./components";
 
-const App = () => {
-  const { status, data, error, isFetching }: any = useUsers();
-
-  return (
-    <div>
-      <h1>Posts</h1>
-      <div>
-        {status === "loading" ? (
-          "Loading..."
-        ) : status === "error" ? (
-          <span>Error: {error.message}</span>
-        ) : (
-          <>
-            <div>Hello world</div>
-            <div>
-              {data.map((user: any) => (
-                <div key={user.id}>
-                  <h1>{user.name}</h1>
-                  <h1>{user.phone}</h1>
-                </div>
-              ))}
-            </div>
-            <div>{isFetching ? "Background Updating..." : " "}</div>
-          </>
-        )}
-      </div>
-    </div>
-  );
-};
+const App = () => (
+  <Content>
+    <List />
+  </Content>
+);
 
 export default App;
